a53977681efe4df849c43493bd80915807faf4c9
mergimalidema <ma32358@ubt-uni.net>
2023-02-04 11:17:15 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,5 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::post('webhook', 'WebhookController@handle');
+Route::post('/webhook', 'WebhookController@handle');
 
