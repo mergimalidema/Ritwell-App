507b9cf63c629fcd144899c289c80b1649bd4c44
mergimalidema <ma32358@ubt-uni.net>
2023-02-06 16:00:06 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 //Route::post('/webhook', [WebhookController::class, 'handle']);
 Route::post('/webhooks/github', function() {
     // Code to handle webhook request and pull changes from Github
-    exec('cd cd /var/www/Ritwell-App && git pull');
+    exec('cd /var/www/Ritwell-App && git pull');
 
 });
 
